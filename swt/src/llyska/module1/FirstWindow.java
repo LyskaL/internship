e0142471b676e0d1d90ca76b40ea6b10d1f02883
Lyska Lyudmila <lucy.daehyun@gmail.com>
2017-01-10 18:49:31 +0200

    Created a new project. Created the first program on SWT.

---
+++ swt/src/llyska/module1/FirstWindow.java
@@ -0,0 +1,37 @@
+package llyska.module1;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
+public class FirstWindow {
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		
+		Shell shell = new Shell(display);
+		shell.open();
+		shell.setText("My First SWT Window!");
+		shell.setBounds(500, 200, 300, 300);
+		
+		Text helloWorldTest = new Text(shell, SWT.NONE);
+		helloWorldTest.setBounds(70, 120, 100, 20);
+		helloWorldTest.setBackground(shell.getBackground());
+		helloWorldTest.setText("My FIRST SWT WINDOW");
+		helloWorldTest.pack();
+		
+		// run the event loop as long as the window is open
+		while (!shell.isDisposed()) {
+		    // read the next OS event queue and transfer it to a SWT event
+		        if (!display.readAndDispatch())
+		         {
+		        // if there are currently no other OS event to process
+		        // sleep until the next OS event is available
+		                display.sleep();
+		         }
+		}
+
+		// disposes all associated windows and their components
+		display.dispose();
+	}
+
+}
