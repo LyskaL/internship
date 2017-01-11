fd61932c9ad3646feeeeaa7413e91097765200e9
Lyska Lyudmila <lucy.daehyun@gmail.com>
2017-01-11 15:23:02 +0200

    Test gridlayout. Added listener on button

---
+++ swt/src/llyska/module1/TestLayout.java
@@ -0,0 +1,63 @@
+package llyska.module1;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+
+public class TestLayout {
+	
+	public TestLayout() {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Test layout");
+		shell.setLayout(new FillLayout());
+		shell.setBounds(800, 300, 500, 500);
+		
+		
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		//gridLayout.makeColumnsEqualWidth = true;
+		shell.setLayout(gridLayout);
+		
+		Button b1 = new Button(shell, SWT.PUSH);
+		b1.setText("B1");
+		b1.setLayoutData(new GridData());
+		GridData grid = new GridData();
+		grid.horizontalAlignment = GridData.FILL;
+		grid.grabExcessHorizontalSpace = true;
+		b1.setLayoutData(grid);
+		
+		new Button(shell, SWT.PUSH).setText("Wide Button 2");
+		new Button(shell, SWT.PUSH).setText("Button 3");
+		
+		Button b4 = new Button(shell, SWT.PUSH);
+		b4.setText("B4");
+        b4.setLayoutData(new GridData (SWT.FILL, SWT.FILL, true, true, 2, 1));
+        b4.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			display.close();
+		}});
+		
+		new Button(shell, SWT.PUSH).setText("Button 5");
+		shell.pack();
+		shell.open();
+		
+		
+		
+		while (!shell.isDisposed()) {
+		        if (!display.readAndDispatch()){
+		               display.sleep();
+		         }
+		}
+		display.dispose();
+		
+		
+	}
+
+	public static void main(String[] args) {
+		new TestLayout();
+	}
+
+}
