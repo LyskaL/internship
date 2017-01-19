d80d03be4e8edc70df26225f894967bcd587865e
Lyska Lyudmila <lucy.daehyun@gmail.com>
2017-01-19 17:19:27 +0200

    Changed code style (TABs replaced on spaces).

--- swt/src/llyska/jface/Table.java
+++ swt/src/llyska/jface/Table.java
@@ -24,116 +24,117 @@ import org.eclipse.swt.widgets.TableColumn;
  */
 public class Table {
 
-	public class MyModel {
-		public int counter;
+    public class MyModel {
+        public int counter;
 
-		public MyModel(int counter) {
-			this.counter = counter;
-		}
+        public MyModel(int counter) {
+            this.counter = counter;
+        }
 
-		@Override
-		public String toString() {
-			return "Item " + this.counter;
-		}
-	}
+        @Override
+        public String toString() {
+            return "Item " + this.counter;
+        }
+    }
 
-	public Table(Shell shell) {
-		final TableViewer v = new TableViewer(shell,SWT.BORDER|SWT.FULL_SELECTION);
-		v.setContentProvider(ArrayContentProvider.getInstance());
+    public Table(Shell shell) {
+        final TableViewer v = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
+        v.setContentProvider(ArrayContentProvider.getInstance());
 
-		TableColumn column = new TableColumn(v.getTable(),SWT.NONE);
-		column.setWidth(100);
-		column.setText("Column 1");
-		TableViewerColumn viewerColumn1 = new TableViewerColumn(v, column);
-		viewerColumn1.setLabelProvider(new ColumnLabelProvider());
-		viewerColumn1.setEditingSupport(new EditColumns(v));
+        TableColumn column = new TableColumn(v.getTable(), SWT.NONE);
+        column.setWidth(100);
+        column.setText("Column 1");
+        TableViewerColumn viewerColumn1 = new TableViewerColumn(v, column);
+        viewerColumn1.setLabelProvider(new ColumnLabelProvider());
+        viewerColumn1.setEditingSupport(new EditColumns(v));
 
-		column = new TableColumn(v.getTable(),SWT.NONE);
-		column.setWidth(100);
-		column.setText("Column 2");
-		TableViewerColumn viewerColumn2 = new TableViewerColumn(v, column);
-		viewerColumn2.setLabelProvider(new ColumnLabelProvider());
-		viewerColumn2.setEditingSupport(new EditColumns(v));
+        column = new TableColumn(v.getTable(), SWT.NONE);
+        column.setWidth(100);
+        column.setText("Column 2");
+        TableViewerColumn viewerColumn2 = new TableViewerColumn(v, column);
+        viewerColumn2.setLabelProvider(new ColumnLabelProvider());
+        viewerColumn2.setEditingSupport(new EditColumns(v));
 
-		MyModel[] model = createModel();
-		v.setInput(model);
-		v.getTable().setLinesVisible(true);
-		v.getTable().setHeaderVisible(true);
+        MyModel[] model = createModel();
+        v.setInput(model);
+        v.getTable().setLinesVisible(true);
+        v.getTable().setHeaderVisible(true);
 
-		v.getTable().addListener(SWT.EraseItem, new Listener() {
+        v.getTable().addListener(SWT.EraseItem, new Listener() {
 
-			@Override
-			public void handleEvent(Event event) {
-				event.detail &= ~SWT.SELECTED;
-			}
-		});
+            @Override
+            public void handleEvent(Event event) {
+                event.detail &= ~SWT.SELECTED;
+            }
+        });
 
-	}
+    }
 
-	private MyModel[] createModel() {
-		MyModel[] elements = new MyModel[10];
+    private MyModel[] createModel() {
+        MyModel[] elements = new MyModel[10];
 
-		for( int i = 0; i < 10; i++ ) {
-			elements[i] = new MyModel(i);
-		}
+        for (int i = 0; i < 10; i++) {
+            elements[i] = new MyModel(i);
+        }
 
-		return elements;
-	}
+        return elements;
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Display display = new Display ();
-		Shell shell = new Shell(display);
-		shell.setLayout(new FillLayout());
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setLayout(new FillLayout());
 
-		final int width = 500;
-		final int height = 400;
-		Monitor monitor = display.getPrimaryMonitor();
-		int x = (monitor.getBounds().width / 2) - width / 2;
-		int y = (monitor.getBounds().height / 2) - height / 2;
-		shell.setBounds(x, y, width, height);
-		
-		
-		new Table(shell);
-		shell.open ();
+        final int width = 500;
+        final int height = 400;
+        Monitor monitor = display.getPrimaryMonitor();
+        int x = (monitor.getBounds().width / 2) - width / 2;
+        int y = (monitor.getBounds().height / 2) - height / 2;
+        shell.setBounds(x, y, width, height);
 
-		while (!shell.isDisposed ()) {
-			if (!display.readAndDispatch ()) display.sleep ();
-		}
+        new Table(shell);
+        shell.open();
 
-		display.dispose ();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
 
-	}
+        display.dispose();
 
-	private class EditColumns extends EditingSupport {
+    }
 
-		public EditColumns(ColumnViewer viewer) {
-			super(viewer);
-		}
+    private class EditColumns extends EditingSupport {
 
-		@Override
-		protected CellEditor getCellEditor(Object element) {
-			return new TextCellEditor((Composite) getViewer().getControl());
-		}
+        public EditColumns(ColumnViewer viewer) {
+            super(viewer);
+        }
 
-		@Override
-		protected boolean canEdit(Object element) {
-			return true;
-		}
+        @Override
+        protected CellEditor getCellEditor(Object element) {
+            return new TextCellEditor((Composite) getViewer().getControl());
+        }
 
-		@Override
-		protected Object getValue(Object element) {
-			return ((MyModel) element).counter + "";
-		}
+        @Override
+        protected boolean canEdit(Object element) {
+            return true;
+        }
 
-		@Override
-		protected void setValue(Object element, Object value) {
-			((MyModel) element).counter = Integer.parseInt(value.toString());
-			getViewer().update(element, null);
-		}
+        @Override
+        protected Object getValue(Object element) {
+            return ((MyModel) element).counter + "";
+        }
 
-	}
+        @Override
+        protected void setValue(Object element, Object value) {
+            ((MyModel) element).counter = Integer.parseInt(value.toString());
+            getViewer().update(element, null);
+        }
+
+    }
 
 }
