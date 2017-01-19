19253ecfdba91fd5d9082a2eb202c154b2c69b33
Lyska Lyudmila <lucy.daehyun@gmail.com>
2017-01-19 17:15:12 +0200

    Changed code style. (TABs replaced on spaces).

--- swt/src/llyska/jface/TestTableViewer.java
+++ swt/src/llyska/jface/TestTableViewer.java
@@ -15,74 +15,71 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TableColumn;
 
 public class TestTableViewer {
-	
-	TableViewer viewer;
-	
-	public TestTableViewer() {
-		Display display = new Display();
-		Shell shell = new Shell(display);
-		shell.setText("JFace homework log");
-		shell.setLayout(new GridLayout(1, true));
-		
-		final int width = 600;
-		final int height = 400;
-		Monitor monitor = display.getPrimaryMonitor();
-		int x = (monitor.getBounds().width / 2) - width / 2;
-		int y = (monitor.getBounds().height / 2) - height / 2;
-		shell.setBounds(x, y, width, height);
-		
-		Composite com = new Composite(shell, SWT.BORDER);
-		com.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		com.setLayout(new FillLayout());
-		
-		createTable(com);
 
-		shell.open();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
-		display.dispose();
-	}
+    TableViewer viewer;
 
-	private void createTable(Composite com) {
-		viewer = new TableViewer(com, SWT.MULTI | SWT.H_SCROLL
-                | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
-		viewer.setContentProvider(ArrayContentProvider.getInstance());
-		
-		
-		String[] titles = { "First name", "Last name", "Gender", "Married" };
-		int[] bounds = { 100, 100, 100, 100 };
+    public TestTableViewer() {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("JFace homework log");
+        shell.setLayout(new GridLayout(1, true));
 
-		// first column is for the first name
-		for (int i = 0; i < titles.length; i++) {
-			TableViewerColumn col = createTableViewerColumn(titles[0], bounds[0], 0);
-			col.setLabelProvider(new ColumnLabelProvider() {
-				@Override
-				public String getText(Object element) {
-					return "4363";
-				}
-			});
-		}
+        final int width = 600;
+        final int height = 400;
+        Monitor monitor = display.getPrimaryMonitor();
+        int x = (monitor.getBounds().width / 2) - width / 2;
+        int y = (monitor.getBounds().height / 2) - height / 2;
+        shell.setBounds(x, y, width, height);
 
-		viewer.getTable().setLinesVisible(true);
-		viewer.getTable().setHeaderVisible(true);
-	}
+        Composite com = new Composite(shell, SWT.BORDER);
+        com.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        com.setLayout(new FillLayout());
 
-	private TableViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
-		TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
-		TableColumn column = viewerColumn.getColumn();
-		column.setText(title);
-		column.setWidth(bound);
-		column.setResizable(true);
-		column.setMoveable(true);
-		return viewerColumn;
-	}
+        createTable(com);
 
-	public static void main(String[] args) {
-		new TestTableViewer();
-	}
-	
-	
+        shell.open();
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+        display.dispose();
+    }
+
+    private void createTable(Composite com) {
+        viewer = new TableViewer(com, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+        viewer.setContentProvider(ArrayContentProvider.getInstance());
+
+        String[] titles = { "First name", "Last name", "Gender", "Married" };
+        int[] bounds = { 100, 100, 100, 100 };
+
+        // first column is for the first name
+        for (String title : titles) {
+            TableViewerColumn col = createTableViewerColumn(titles[0], bounds[0], 0);
+            col.setLabelProvider(new ColumnLabelProvider() {
+                @Override
+                public String getText(Object element) {
+                    return "4363";
+                }
+            });
+        }
+
+        viewer.getTable().setLinesVisible(true);
+        viewer.getTable().setHeaderVisible(true);
+    }
+
+    private TableViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
+        TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+        TableColumn column = viewerColumn.getColumn();
+        column.setText(title);
+        column.setWidth(bound);
+        column.setResizable(true);
+        column.setMoveable(true);
+        return viewerColumn;
+    }
+
+    public static void main(String[] args) {
+        new TestTableViewer();
+    }
+
 }
