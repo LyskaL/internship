9d9983a214faeb0a726752b78c787949320f102e
Lyska Lyudmila <lucy.daehyun@gmail.com>
2017-02-20 19:01:04 +0200

    Added new actions: new, save, delete and cancel file. Also added icon to them.

---
+++ StudentsRCP/src/actions/CancelAction.java
@@ -0,0 +1,53 @@
+package actions;
+
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+
+public class CancelAction extends Action implements ISelectionListener, ActionFactory.IWorkbenchAction {
+
+    private final IWorkbenchWindow _window;
+    public final static String ID = "StudentsRCP.actions.newAction";
+    private IStructuredSelection _selection;
+
+    public CancelAction(IWorkbenchWindow window) {
+        this._window = window;
+        setId(ID);
+        setText("&Cancel");
+        setToolTipText("Clear form panel...");
+
+        URL url = Platform.getBundle("StudentsRCP").getEntry("icons/cancel_icon.png");
+        setImageDescriptor(ImageDescriptor.createFromURL(url));
+    }
+
+    @Override
+    public void dispose() {
+        _window.getSelectionService().removeSelectionListener(this);
+    }
+
+    @Override
+    public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+        // TODO Auto-generated method stub
+        // selection containing elements
+        // if (incoming instanceof IStructuredSelection) {
+        // selection = (IStructuredSelection) incoming;
+        // setEnabled(selection.size() == 1 && selection.getFirstElement() instanceof ContactsGroup);
+        // } else {
+        // // other selections (e.g., containing text or of other kinds)
+        // setEnabled(false);
+        // }
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
---
+++ StudentsRCP/src/actions/DeleteAction.java
@@ -0,0 +1,47 @@
+package actions;
+
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+
+public class DeleteAction extends Action implements ISelectionListener, ActionFactory.IWorkbenchAction {
+
+    private final IWorkbenchWindow _window;
+    public final static String ID = "StudentsRCP.actions.newAction";
+    private IStructuredSelection _selection;
+
+    public DeleteAction(IWorkbenchWindow window) {
+        this._window = window;
+        setId(ID);
+        setText("&Delete");
+        setToolTipText("Delete information from table");
+
+        URL url = Platform.getBundle("StudentsRCP").getEntry("icons/delete_icon.png");
+        setImageDescriptor(ImageDescriptor.createFromURL(url));
+    }
+
+    @Override
+    public void dispose() {
+        _window.getSelectionService().removeSelectionListener(this);
+    }
+
+    @Override
+    public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+}
