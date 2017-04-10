00fa25a5d48c1320e34cc3617b3d33f657c9a1f1
Lyska Lyudmila <lucy.daehyun@gmail.com>
2017-04-10 12:19:31 +0300

    The first parser for a xml file

---
+++ XML/src/xml/parser/DomParserDemo.java
@@ -0,0 +1,42 @@
+package xml.parser;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class DomParserDemo {
+    public static void main(String[] args) {
+
+        try {
+            File inputFile = new File("D:/internship/src/internship/XML/files/xml.example1.txt");
+            System.out.println(inputFile.getAbsolutePath());
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("customer");
+            System.out.println("----------------------------");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Student roll no : " + eElement.getAttribute("rollno"));
+                    System.out.println(
+                            "First Name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    System.out.println(
+                            "Last Name : " + eElement.getElementsByTagName("age").item(0).getTextContent());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
